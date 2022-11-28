7f661f4e7c06fd40c5f0e127e099d4b3408538c7
徐超越 <993921@qq.com>
2022-11-28 20:24:15 +0800

    Update Tpl.vue

--- src/views/Projects/CI/components/Tpl.vue
+++ src/views/Projects/CI/components/Tpl.vue
@@ -1,14 +1,34 @@
-<template>
-  <ElSpace />
-</template>
-
 <script setup lang="ts">
+import { reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from '@/hooks/web/useI18n'
 
 const { t } = useI18n()
 const { path, params } = useRoute()
 const { push } = useRouter()
+
+const formPlan = reactive({
+  name: '',
+  region: '',
+  type: ''
+})
 </script>
+
+<template>
+  <ElForm label-position="top" :model="formPlan">
+    <ElTimeline>
+      <ElTimelineItem timestamp="abc" :icon="material-symbols:filter-1" placement="top">
+        <ElCard>
+          <ElFormItem label="计划名称"> <ElInput v-model="formPlan.name" /> </ElFormItem>
+        </ElCard> </ElTimelineItem
+      ><ElTimelineItem timestamp="def" placement="top">
+        <ElCard>
+          <ElFormItem label="代码仓库"> <ElInput v-model="formPlan.name" /> </ElFormItem>
+          <ElFormItem label="构建分支"> <ElInput v-model="formPlan.name" /> </ElFormItem>
+        </ElCard>
+      </ElTimelineItem>
+    </ElTimeline>
+  </ElForm>
+</template>
 
 <style scoped></style>
